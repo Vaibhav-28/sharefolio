2060771052eb40e4fba6de79e8efe6b284ea43ea
Vaibhav Singh Sisodia <51284690+Vaibhav-28@users.noreply.github.com>
2024-11-14 23:31:15 +0530

    handled text overflow in upload preview

--- app/(dashboard)/(routes)/upload/_components/FilePreview.tsx
+++ app/(dashboard)/(routes)/upload/_components/FilePreview.tsx
@@ -9,18 +9,21 @@ interface FilePreviewProps {
 
 const FilePreview = ({ file, removeFile }: FilePreviewProps) => {
   return (
-    <div className="flex items-center gap-2 justify-between mt-5 p-2 border rounded-md border-blue-200">
-      <div className="flex items-center p-2">
+    <div className="flex items-center justify-between gap-2 mt-5 p-2 border rounded-md border-blue-200 overflow-hidden">
+      <div className="flex items-center flex-1 min-w-0">
         <Image src="/file.png" width={50} height={50} alt="file" />
-        <div className="text-left">
-          <h2>{file?.name}</h2>
-          <h2 className="text-[12px] text-gray-400">
+        <div className="text-left ml-2 flex-1 min-w-0">
+          <h2 className="truncate">{file?.name}</h2>
+          <h2 className="text-[12px] text-gray-400 truncate">
             {file?.type} /{" "}
-            {file?.size ? (file?.size / 10224 / 1024).toFixed(2) : ""} MB
+            {file?.size ? (file?.size / 1024 / 1024).toFixed(2) : ""} MB
           </h2>
         </div>
       </div>
-      <X onClick={removeFile} className="text-red-500 cursor-pointer" />
+      <X
+        onClick={removeFile}
+        className="text-red-500 cursor-pointer flex-shrink-0"
+      />
     </div>
   );
 };
